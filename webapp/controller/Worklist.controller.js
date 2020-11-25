dfe0bcca9c56595763d927ff0318ef397a6d1bb6
5v9yK257Edpj7eR <matteo.convertino@accenture.com>
2020-11-25 22:13:16 +0000

    w4u1

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -65,7 +65,13 @@ sap.ui.define([
 		/* =========================================================== */
 		/* event handlers                                              */
 		/* =========================================================== */
-
+		/**
+		 * Event handler when the add button gets pressed
+		 * @public
+		 */
+		onAdd: function () {
+			this.getRouter().navTo("add");
+		},
 		/**
 		 * Event handler when a filter tab gets pressed
 		 * @param {sap.ui.base.Event} oEvent the filter tab event
